bda7baf5161f0b2fa08dad25976a7523e18a10c2
Alan <alancastillejos3@gmail.com>
2026-08-24 02:29:54 -0600

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <atomic>
+#include <cmath>
+#include <chrono>
+
+#define MINIAUDIO_IMPLEMENTATION
+//#include "miniaudio.h"
+//#include "whisper.h"
+
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
